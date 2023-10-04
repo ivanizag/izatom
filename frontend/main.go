18625a7029a299984c8215880774f826c2d1ceac
Ivan Izaguirre <ivanizag@gmail.com>
2023-10-05 00:02:38 +0200

    Crude text mode. The emulator is usable!

--- frontend/main.go
+++ frontend/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"unsafe"
+
 	"github.com/ivanizag/izatom"
 	"github.com/veandco/go-sdl2/sdl"
 )
@@ -13,7 +15,7 @@ func main() {
 	go a.Run()
 
 	// Prepare SDL
-	window, renderer, err := sdl.CreateWindowAndRenderer(500, 300,
+	window, renderer, err := sdl.CreateWindowAndRenderer(256*4, 192*4,
 		sdl.WINDOW_SHOWN)
 	if err != nil {
 		panic(err)
@@ -37,8 +39,31 @@ func main() {
 		}
 
 		// Draw
-		renderer.Clear()
-		renderer.Present()
+		img := a.Snapshot()
+
+		if img != nil {
+			surface, err := sdl.CreateRGBSurfaceFrom(unsafe.Pointer(&img.Pix[0]),
+				int32(img.Bounds().Dx()), int32(img.Bounds().Dy()),
+				32, 4*img.Bounds().Dx(),
+				0x0000ff, 0x0000ff00, 0x00ff0000, 0xff000000)
+			// Valid for little endian. Should we reverse for big endian?
+			// 0xff000000, 0x00ff0000, 0x0000ff00, 0x000000ff)
+
+			if err != nil {
+				panic(err)
+			}
+
+			texture, err := renderer.CreateTextureFromSurface(surface)
+			if err != nil {
+				panic(err)
+			}
+
+			renderer.Clear()
+			renderer.Copy(texture, nil, nil)
+			renderer.Present()
+			surface.Free()
+			texture.Destroy()
+		}
 		sdl.Delay(1000 / 30)
 	}
 
